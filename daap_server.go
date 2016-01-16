0bcf5236c9657b2a31aa51577a935b8e9ac03dfd
Carl Green <carlgreen@gmail.com>
2016-01-16 15:49:28 +1300

    initial response for content-codes

--- daap_server.go
+++ daap_server.go
@@ -5,12 +5,44 @@ import (
 	"net/http"
 )
 
+func contentCodeToInt(contentCode string) int {
+	return (int(contentCode[0]&0xFF) << 24) |
+		(int(contentCode[1]&0xFF) << 16) |
+		(int(contentCode[2]&0xFF) << 8) |
+		int(contentCode[3]&0xFF)
+}
+
+func intToByteArray(i int) []byte {
+	data := [4]byte{
+		byte((i >> 24) & 0xFF),
+		byte((i >> 16) & 0xFF),
+		byte((i >> 8) & 0xFF),
+		byte(i & 0xFF),
+	}
+	return data[:]
+}
+
 func serverInfoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add(`DAAP-Server`, `daap-server: 1.0`)
 	fmt.Fprintf(w, "hello, world\n")
 }
 
+func contentCodesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add(`DAAP-Server`, `daap-server: 1.0`)
+	// data := []byte{109, 99, 99, 114, 0, 0, 0, 73, 109, 115, 116, 116, 0, 0, 0, 4, 0, 0, 0, 200}
+	data := []byte{}
+	data = append(data, intToByteArray(contentCodeToInt("mccr"))...)
+	data = append(data, intToByteArray(12)...)
+
+	data = append(data, intToByteArray(contentCodeToInt("mstt"))...)
+	data = append(data, intToByteArray(4)...)
+	data = append(data, intToByteArray(200)...)
+
+	w.Write(data)
+}
+
 func main() {
 	http.HandleFunc("/server-info", serverInfoHandler)
+	http.HandleFunc("/content-codes", contentCodesHandler)
 	http.ListenAndServe(":3689", nil)
 }
--- daap_server_test.go
+++ daap_server_test.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func TestContentCodeToInt(t *testing.T) {
+	i := contentCodeToInt("mccr")
+	if i != 1835230066 {
+		t.Errorf("wrong integer value for content code: %v", i)
+	}
+}
+
+func TestIntToByteArray(t *testing.T) {
+	b := intToByteArray(1835230066)
+	if !bytes.Equal(b, []byte{109, 99, 99, 114}) {
+		t.Errorf("wrong byte array value for int: %v", b)
+	}
+}
 
 func TestGetServerInfo(t *testing.T) {
 	req, err := http.NewRequest("GET", "", nil)
@@ -30,5 +45,31 @@ func TestGetServerInfo(t *testing.T) {
 	}
 	if !strings.Contains(string(p), `hello, world`) {
 		t.Errorf("response body doen't match:\n%s", p)
+	}
+}
+
+func TestGetContentCodes(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp := httptest.NewRecorder()
+	contentCodesHandler(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("wrong http status, want %v, got %v", http.StatusOK, resp.Code)
+	}
+	p, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	serverHeader := resp.HeaderMap.Get("DAAP-Server")
+	if serverHeader == "" {
+		t.Error("did not contain DAAP-Server header")
+	} else if !strings.Contains(serverHeader, `daap-server`) {
+		t.Errorf("DAAP-Server header doesn't match:\n%s", serverHeader)
+	}
+	expectedData := []byte{109, 99, 99, 114, 0, 0, 0, 12, 109, 115, 116, 116, 0, 0, 0, 4, 0, 0, 0, 200}
+	if !bytes.Equal(p, expectedData) {
+		t.Errorf("response body doen't match:\n%v", p)
 	}
 }
